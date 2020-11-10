999b7fc20d75b5fdad59acc78d9dbb9857da599d
--replace-all <1164@holbertonschool.com>
2020-11-10 03:31:33 +0000

    implement add vertex functionality

---
+++ 0x01-graphs/1-graph_add_vertex.c
@@ -0,0 +1,45 @@
+#include "graphs.h"
+
+/**
+ * graph_add_vertex - add a vertex to the graph
+ * @graph: is the given graph structure
+ * @str: is the content of te new vertex
+ * Return: the position of the new vertex or null on error
+ */
+vertex_t *graph_add_vertex(graph_t *graph, const char *str)
+{
+	vertex_t *newVertex = NULL, *index = NULL;
+
+	if (!graph)
+		return (NULL);
+
+	newVertex = calloc(sizeof(*newVertex), 1);
+	if (!newVertex)
+		return (NULL);
+
+	newVertex->content = strdup(str);
+	index = graph->vertices;
+
+	if (!index)
+	{
+		graph->vertices = newVertex;
+		graph->nb_vertices++;
+		return (newVertex);
+	}
+
+	while (index)
+	{
+		if (!strcmp(index->content, newVertex->content))
+			return (NULL);
+		if (!index->next)
+		{
+			index->next = newVertex;
+			newVertex->index = index->index + 1;
+			graph->nb_vertices++;
+			return (newVertex);
+		}
+		index = index->next;
+	}
+
+	return (NULL);
+}
