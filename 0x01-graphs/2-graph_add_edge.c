37662c5391e6c54d28976561f318e6d9ed3b595f
--replace-all <1164@holbertonschool.com>
2020-11-21 20:24:13 +0000

    create and insertion functions

--- 0x01-graphs/2-graph_add_edge.c
+++ 0x01-graphs/2-graph_add_edge.c
@@ -1,6 +1,63 @@
 #include "graphs.h"
 
 /**
+ * insertEdge - insert a new edge to the edges list of the src vertix
+ * @src: is the vertex that has the edge
+ * @newEdge: is the new edge to be added
+ * Return: 1 in success, 0 on failure
+ */
+int insertEdge(vertex_t *src, edge_t *newEdge)
+{
+	edge_t *index = NULL;
+
+	if (!src || !newEdge)
+		return (0);
+
+	index = src->edges;
+
+	if (!index)
+	{
+		src->edges = newEdge;
+		return (1);
+	}
+
+	while (index->next)
+	{
+		index = index->next;
+	}
+
+	index->next = newEdge;
+	return (1);
+}
+
+/**
+ * createEdge - creates a new edge from the given src to dest vertex
+ * @src: is the origin vertex
+ * @dest: is the destination vertex
+ * Return: a pointer to the new edge, or NULL on failure
+ */
+edge_t *createEdge(vertex_t *src, vertex_t *dest)
+{
+	edge_t *newEdge = NULL;
+
+	if (!src || !dest)
+		return (NULL);
+
+	newEdge = malloc(sizeof(*newEdge));
+	if (!newEdge)
+		return (NULL);
+	newEdge->dest = dest;
+	newEdge->next = NULL;
+	if (!insertEdge(src, newEdge))
+	{
+		free(newEdge);
+		return (NULL);
+	}
+
+	return (newEdge);
+}
+
+/**
  * graph_add_edge - add an edge to the graph
  * @graph: is the given graph structure
  * @src: is the content of te new vertex
@@ -12,6 +69,7 @@ int graph_add_edge(graph_t *graph, const char *src, const char *dest,
 				   edge_type_t type)
 {
 	vertex_t *source = NULL, *destination = NULL, *index = NULL;
+	edge_t *sourceDirection = NULL, *destinationDirection = NULL;
 
 	if (EARLY_RETURN(graph, src, dest, type))
 		return (0);
@@ -28,6 +86,21 @@ int graph_add_edge(graph_t *graph, const char *src, const char *dest,
 	}
 	if (!source || !destination)
 		return (0);
+
+	sourceDirection = createEdge(source, destination);
+	if (!sourceDirection)
+		return (0);
+
+	if (type == BIDIRECTIONAL)
+	{
+		destinationDirection = createEdge(destination, source);
+		if (!destinationDirection)
+		{
+			free(sourceDirection);
+			sourceDirection = NULL;
+			return (0);
+		}
+	}
 
 	return (1);
 }
