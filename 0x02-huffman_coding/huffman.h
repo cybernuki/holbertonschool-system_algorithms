e6af4b80625e518f32cb8943186a91e79e6968d7
cybernuki <1164@holbertonschool.com>
2020-12-08 21:20:17 +0000

    priority queue

--- 0x02-huffman_coding/huffman.h
+++ 0x02-huffman_coding/huffman.h
@@ -2,6 +2,7 @@
 #define HUFFMAN_H
 
 #include <stdlib.h>
+#include "heap/heap.h"
 
 /**
  * struct symbol_s - Stores a char and its associated frequency
@@ -16,5 +17,6 @@ typedef struct symbol_s
 } symbol_t;
 
 symbol_t *symbol_create(char data, size_t freq);
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 
 #endif
---
+++ 0x02-huffman_coding/huffman_priority_queue.c
@@ -0,0 +1,55 @@
+#include "huffman.h"
+
+/**
+ * int_cmp - Compares two integers
+ *
+ * @p1: First pointer
+ * @p2: Second pointer
+ *
+ * Return: Difference between the two strings
+ */
+int int_cmp(void *p1, void *p2)
+{
+	symbol_t *n1, *n2;
+
+	n1 = ((binary_tree_node_t *)p1)->data;
+	n2 = ((binary_tree_node_t *)p2)->data;
+	return (n1->freq - n2->freq);
+}
+
+/**
+ * huffman_priority_queue - build a priority queue
+ * @data: is an array of characters
+ * @freq: is an array containing the associated frequencies
+ * @size: is the size of both arrays
+ * Return: a new minumun heap, NULL on failure
+ */
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
+{
+	heap_t *queue = NULL;
+	binary_tree_node_t *nested = NULL;
+	symbol_t *symbol = NULL;
+	size_t index = 0;
+
+	if (!data || !freq)
+		return (NULL);
+
+	queue = heap_create(int_cmp);
+	if (!queue)
+		return (NULL);
+
+	while (index < size)
+	{
+		symbol = symbol_create(data[index], freq[index]);
+		if (!symbol)
+			return (NULL);
+		nested = malloc(sizeof(*nested));
+		if (!nested)
+			return (NULL);
+		nested->data = symbol;
+		if (!heap_insert(queue, nested))
+			return (NULL);
+		index++;
+	}
+	return (queue);
+}
