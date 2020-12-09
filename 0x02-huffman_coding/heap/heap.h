dc4776f8c4b5680a06d504cd638b65cadf02ab94
cybernuki <1164@holbertonschool.com>
2020-12-09 04:23:11 +0000

    style

--- 0x02-huffman_coding/heap/heap.h
+++ 0x02-huffman_coding/heap/heap.h
@@ -13,10 +13,10 @@
  */
 typedef struct binary_tree_node_s
 {
-    void *data;
-    struct binary_tree_node_s *left;
-    struct binary_tree_node_s *right;
-    struct binary_tree_node_s *parent;
+	void *data;
+	struct binary_tree_node_s *left;
+	struct binary_tree_node_s *right;
+	struct binary_tree_node_s *parent;
 } binary_tree_node_t;
 
 /**
@@ -28,14 +28,14 @@ typedef struct binary_tree_node_s
  */
 typedef struct heap_s
 {
-    size_t size;
-    int (*data_cmp)(void *, void *);
-    binary_tree_node_t *root;
+	size_t size;
+	int (*data_cmp)(void *, void *);
+	binary_tree_node_t *root;
 } heap_t;
 
 heap_t *heap_create(int (*data_cmp)(void *, void *));
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent,
-                                     void *data);
+									 void *data);
 binary_tree_node_t *heap_insert(heap_t *heap, void *data);
 void *heap_extract(heap_t *heap);
 void heap_delete(heap_t *heap, void (*free_data)(void *));
