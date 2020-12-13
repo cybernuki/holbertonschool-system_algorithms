9aaf49c1cce00adf82ede9256dc9aa7db6b8080e
cybernuki <1164@holbertonschool.com>
2020-12-13 21:37:44 +0000

    fix

---
+++ 0x02-huffman_coding/huffman/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char argv[])
+{
+    if (argc != 4)
+    {
+        fprintf(stderr, "Usage: huffman <mode> <filename> <out_filename>\n");
+        return (EXIT_FAILURE);
+    }
+
+    return (EXIT_SUCCESS);
+}
