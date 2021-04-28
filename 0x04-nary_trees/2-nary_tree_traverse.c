6cbf72782a272845feb4f29ae6968e0e98b1264d
cybernuki <1164@holbertonschool.com>
2021-04-28 23:09:26 +0000

    fix

--- 0x04-nary_trees/2-nary_tree_traverse.c
+++ 0x04-nary_trees/2-nary_tree_traverse.c
@@ -7,11 +7,12 @@
  * Return: Largest depth traversed
  */
 size_t nary_tree_traverse(nary_tree_t const *root,
-                          void (*action)(nary_tree_t const *node, size_t depth))
+						  void (*action)(nary_tree_t const *node,
+										 size_t depth))
 {
-    if (!root || !action)
-        return (0);
-    return (_nary_tree_traverse(root, action, 0));
+	if (!root || !action)
+		return (0);
+	return (_nary_tree_traverse(root, action, 0));
 }
 
 /**
@@ -22,18 +23,18 @@ size_t nary_tree_traverse(nary_tree_t const *root,
  * Return: Largest depth traversed
  */
 size_t _nary_tree_traverse(nary_tree_t const *root,
-                           void (*action)(nary_tree_t const *node,
-                                          size_t depth),
-                           size_t depth)
+						   void (*action)(nary_tree_t const *node,
+										  size_t depth),
+						   size_t depth)
 {
-    size_t max_depth = depth, temp_depth;
+	size_t max_depth = depth, temp_depth;
 
-    if (!root)
-        return (0);
-    action(root, depth);
-    temp_depth = _nary_tree_traverse(root->children, action, depth + 1);
-    max_depth = MAX(max_depth, temp_depth);
-    temp_depth = _nary_tree_traverse(root->next, action, depth);
-    max_depth = MAX(max_depth, temp_depth);
-    return (max_depth);
+	if (!root)
+		return (0);
+	action(root, depth);
+	temp_depth = _nary_tree_traverse(root->children, action, depth + 1);
+	max_depth = MAX(max_depth, temp_depth);
+	temp_depth = _nary_tree_traverse(root->next, action, depth);
+	max_depth = MAX(max_depth, temp_depth);
+	return (max_depth);
 }
